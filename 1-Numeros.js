dc54def19b8ec7e7f334d1311d2675bb84c66ee7
diego sierra <diegofsierra109@gmail.com>
2024-07-31 14:48:23 -0500

    aula1

--- 1-Numeros.js
+++ 1-Numeros.js
@@ -1,2 +1,16 @@
-var primerNumero = 3;
-console.log(primerNumero);
+var primerNumero = 3.5;
+var segundoNumero = 25.5;
+
+// operaciones aritmetica con numeros
+var sumaNumeros = primerNumero + segundoNumero;
+var restaNumeros = primerNumero - segundoNumero;
+var multiplicacionNumeros = primerNumero * segundoNumero;
+var divisionNumeros = primerNumero / segundoNumero;
+
+console.log(`la suma fue:`,sumaNumeros);
+console.log(`la resta fue:`,restaNumeros);
+console.log(`la multiplicacion fue:`,parseInt(multiplicacionNumeros));
+console.log(`la division fue:`,divisionNumeros.toFixed(2));
+
+var numeroMasLetras = primerNumero - "Alura latam";
+console.log(numeroMasLetras);
