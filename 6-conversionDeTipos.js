5ed5b0f9b34e7b7af6522e4e6ece5b630b7254ab
diego sierra <diegofsierra109@gmail.com>
2024-08-01 17:18:54 -0500

    Aula2

---
+++ 6-conversionDeTipos.js
@@ -0,0 +1,10 @@
+// convesion implicita 
+
+const edad = 39; 
+const edadUsuario = "39a";
+
+console.log(edad.toString() + edadUsuario);
+
+// conversion explicita
+
+console.log(100 + edad + Number(edadUsuario));
