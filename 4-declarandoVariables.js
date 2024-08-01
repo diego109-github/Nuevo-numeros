b5e7fb15d28c56b93132c2d7fca28d217e18bf5e
diego sierra <diegofsierra109@gmail.com>
2024-08-01 10:39:21 -0500

    aula1

---
+++ 4-declarandoVariables.js
@@ -0,0 +1,14 @@
+// programa para calcular el area de un rectangulo 
+
+{
+        const base = 10;
+        const altura = 20;
+        const area = base * altura;
+    {
+        console.log(area);
+    }
+    altura = 15;
+    area= base*altura;
+    console.log(area);
+
+}
