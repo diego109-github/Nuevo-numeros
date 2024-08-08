65840043c6129a22318c72b376f4a79bd88e9701
Diego Sierra <diegofsierra109@gmail.com>
2024-08-08 15:07:59 -0500

    Aula5

--- 18-arrow.js
+++ 18-arrow.js
@@ -1,29 +1,13 @@
-Function multiplicaNumero(a, b, c){
-    return  a*b*c;
-    return resultado;
+function multiplicaNumero(a, b, c){
+    return  a*b*c; 
 }
-
+ 
 // exprecion de funcion
-const multiplicaNumero = function (a, b, c){
+const multiplicaNumero = function (a, b, c){  
     return  a*b*c;
 }
 
 // arrow function
 const multiplicaNumeroFuncionFlecha = (a,b,c) => a*b*c;
 
-const potenciaDe = numero => numero*numero;
-
-{
-    const function1 = function(){
-        // this-1
-        function function2 (){
-            // this-2
-        }
-    } 
-    const functionF = () =>{
-        // this-1
-        const functionF = () => {
-            // this-2
-        }
-    } 
-}
+const potenciaDe2 = numero => numero*numero;
